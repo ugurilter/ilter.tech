b4c4714a4237d7491a60dde08bed64f5ce8f9300
Ugur Ilter <ugur@ilter.tech>
2023-11-28 22:38:48 +0300

    improve status and activity

--- src/utils/lanyardSocket.js
+++ src/utils/lanyardSocket.js
@@ -34,11 +34,12 @@ function update_user_data(j) {
     }
 
     if (dc_activity) {
-        const activityElement = document.getElementById('activity');
+        const activityTypeElement = document.getElementById('activity-type');
 
-        if (activityElement && dc_activity.name === 'YouTube Music') {
-            activityElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" /></svg>';
-            activityElement.innerHTML += 'Listening to: ' + dc_activity.state + ' - ' + dc_activity.details;
+        if (dc_activity.name === 'YouTube Music') {
+            activityTypeElement.innerHTML =  '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" /></svg>';
+            activityTypeElement.innerHTML += 'Listening: ';
+            activityTypeElement.innerHTML += '<marquee scrollamount=3 behavior="scroll">' + dc_activity.state + ' - ' + dc_activity.details + '</marquee>'
         } else if (activityElement && dc_activity.name === 'Custom Status') {
             activityElement.innerHTML =  dc_activity.state + dc_activity.emoji.name;
         } else if (activityElement && !dc_activity.state && !dc_activity.details) {
@@ -48,7 +49,7 @@ function update_user_data(j) {
             // activityElement.innerHTML += 'https://cdn.discordapp.com/app-icons/' + dc_activity.application_id + '/' + dc_activity.assets.large_image + '.webp?size=40&keep_aspect_ratio=false';
         }
     } else {
-        const activityElement = document.getElementById('activity');
+        const activityElement = document.getElementById('activity-type');
 
         if (activityElement) {
             activityElement.innerHTML = 'Doing nothing...';
